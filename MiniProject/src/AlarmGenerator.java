64d38f8290b0b1e94bc7a2470d9d64f7bcbcf7cc
abderrahman.boudrai <abderrahman.boudrai@DESK0027.tecsidel.es>
2018-03-22 13:17:35 +0100

    c333

--- MiniProject/src/AlarmGenerator.java
+++ MiniProject/src/AlarmGenerator.java
@@ -52,7 +52,7 @@ public class AlarmGenerator extends JFrame {
 		
 		comboBox1.addItem("");
 
-		comboBox1.addItem("c120");
+		comboBox1.addItem("c333");
 		comboBox1.addItem("c110");
 		comboBox1.addItem("c119");
 		
